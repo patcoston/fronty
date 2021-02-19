298e337285b483cdb1764ee96893eb8a290fce95
Patrick Coston <patcoston@gmail.com>
2021-02-18 23:49:04 -0500

    Refactored EducationRequired with FilterChoose

--- src/components/filters/FilterEducationRequired.tsx
+++ src/components/filters/FilterEducationRequired.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react"
+import FilterChoose from "../FilterChoose"
 import { useStore } from "../../store/StoreProvider"
 import { education } from "../../data/education"
 import "./FilterEducationRequired.css"
@@ -10,31 +11,12 @@ interface Props {
 const FilterEducationRequired: FC<Props> = ({ showFilter }) => {
   const { filterShowEducationRequired } = useStore()
   return (
-    <>
-      <div
-        style={
-          !filterShowEducationRequired
-            ? { display: "none" }
-            : { display: "block" }
-        }
-        className="filter-education-required-body"
-      >
-        <label htmlFor="choose-education-required">
-          Filter on Education Requied
-        </label>
-        <select
-          id="choose-education-required"
-          className="filter-education-required-list"
-          multiple
-          size={9}
-        >
-          {education.map(ed => (
-            <option value={ed}>{ed}</option>
-          ))}
-        </select>
-        <button onClick={() => showFilter("ShowResults")}>Submit Filter</button>
-      </div>
-    </>
+    <FilterChoose
+      showFilter={showFilter}
+      show={filterShowEducationRequired}
+      list={education}
+      label={"Education Required"}
+    />
   )
 }
 
