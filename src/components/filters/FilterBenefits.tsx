3a1a1aed41b0ce584d0c47547118cf9f22481127
Patrick Coston <patcoston@gmail.com>
2021-02-18 21:32:52 -0500

    FilterBenefits: Added selection UI

--- src/components/filters/FilterBenefits.tsx
+++ src/components/filters/FilterBenefits.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react"
 import { useStore } from "../../store/StoreProvider"
+import { benefits } from "../../data/benefits"
 import "./FilterBenefits.css"
 
 interface Props {
@@ -14,7 +15,17 @@ const FilterBenefits: FC<Props> = ({ showFilter }) => {
         style={!filterShowBenefits ? { display: "none" } : { display: "block" }}
         className="filter-benefits-body"
       >
-        Filter Benefits
+        <label htmlFor="choose-accessibility">Filter on Accessibility</label>
+        <select
+          id="choose-accessibility"
+          className="filter-skill-list"
+          multiple
+          size={9}
+        >
+          {benefits.map(benefit => (
+            <option value="{access}">{benefit}</option>
+          ))}
+        </select>
         <button onClick={() => showFilter("ShowResults")}>Submit Filter</button>
       </div>
     </>
