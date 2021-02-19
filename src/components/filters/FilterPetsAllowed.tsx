28ed2c0bf20b2c64b46ca9ddbd47702856280feb
Patrick Coston <patcoston@gmail.com>
2021-02-18 22:50:59 -0500

    FilterPetsAllowed: Added change UI

--- src/components/filters/FilterPetsAllowed.tsx
+++ src/components/filters/FilterPetsAllowed.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react"
 import { useStore } from "../../store/StoreProvider"
+import { pets } from "../../data/pets"
 import "./FilterPetsAllowed.css"
 
 interface Props {
@@ -16,7 +17,17 @@ const FilterPetsAllowed: FC<Props> = ({ showFilter }) => {
         }
         className="filter-pets-allowed-body"
       >
-        Filter Pets Allowed
+        <label htmlFor="choose-pets-allowed">Filter on Pets Allowed</label>
+        <select
+          id="choose-pets-allowed"
+          className="filter-pets-allowed-list"
+          multiple
+          size={5}
+        >
+          {pets.map(val => (
+            <option value={val}>{val}</option>
+          ))}
+        </select>
         <button onClick={() => showFilter("ShowResults")}>Submit Filter</button>
       </div>
     </>
