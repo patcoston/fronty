e8e6bb3e6adbfe7927a1586afb0bf67abe5d30ec
Patrick Coston <patcoston@gmail.com>
2021-02-26 20:59:27 -0500

    Show Results: Show random company names

--- src/components/ShowResults.tsx
+++ src/components/ShowResults.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react"
 import { useStore } from "../store/StoreProvider"
+import filterData from "../data/filter-data"
+import { SHOW_FILTER_COMPANY } from "../utils/constants"
 import "./ShowResults.css"
 
 const ShowResults: FC = () => {
@@ -7,7 +9,22 @@ const ShowResults: FC = () => {
   const activeFilterArray = Object.entries(activeFilters)
   const filtersToShow: boolean = activeFilterArray.length > 0
   const show: boolean = showResults && filtersToShow
-  return <>{show && <div className="show-results">Show Results</div>}</>
+  const companies: Array<string> = []
+  const len = filterData[SHOW_FILTER_COMPANY].list.length
+  for (let i: number = 0; i < 10; i++) {
+    let rnd = Math.floor(Math.random() * Math.floor(len - 1)) + 1
+    companies.push(filterData[SHOW_FILTER_COMPANY].list[rnd])
+  }
+
+  return (
+    <>
+      {show && (
+        <div className="show-results">
+          {companies.map(company => `${company} `)}
+        </div>
+      )}
+    </>
+  )
 }
 
 export default ShowResults
