8705e2170cb379bdb8894905a0abd18d816cf889
Patrick Coston <patcoston@gmail.com>
2021-03-01 18:31:29 -0500

    Set max width to 600px

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from "react"
 import MainBody from "./components/MainBody"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <MainBody />
-      <Footer />
+    <div className="app-wrapper">
+      <div className="app-body">
+        <Header />
+        <MainBody />
+        <Footer />
+      </div>
     </div>
   )
 }
