fc07dbe826e482260e2f0e5ada28e00579e25de3
Patrick Coston <patcoston@gmail.com>
2021-03-01 17:49:26 -0500

    Removed App.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import MainBody from "./components/MainBody"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import "./App.css"
 
 function App() {
   return (
