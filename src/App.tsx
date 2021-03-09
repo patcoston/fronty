35d3075c9880c827a7d3a21d5cfd0c59981baa9e
Patrick Coston <patcoston@gmail.com>
2021-03-09 12:09:34 -0500

    Moved data read for Filter Type menu from FilterTypeMenu to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
 import React from "react"
+import { useStore } from "./store/StoreProvider"
 import Routes from "./components/Routes/Routes"
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
+import axios from "axios"
+import * as constant from "./utils/constants"
+import { TFilterDataArray } from "./utils/types"
 import "./App.scss"
 
-function App() {
+const App = () => {
+  const { setFilterData } = useStore()
+  const jsonData: TFilterDataArray = []
+
+  const readData = async (data: string, index: number) => {
+    try {
+      const response = await axios.get(`${constant.URL_DATA}${data}.json`)
+      jsonData[index] = await response.data
+      console.log(jsonData)
+      setFilterData(jsonData)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  readData("techstack", constant.SHOW_FILTER_TECH_STACK)
+  readData("workingfromhome", constant.SHOW_FILTER_WORKING_FROM_HOME)
+  readData("payment", constant.SHOW_FILTER_PAYMENT)
+  readData("benefits", constant.SHOW_FILTER_BENEFITS)
+  readData("commute", constant.SHOW_FILTER_COMMUTE)
+  readData("company", constant.SHOW_FILTER_COMPANY)
+  readData("companysize", constant.SHOW_FILTER_COMPANY_SIZE)
+  readData("educationrequired", constant.SHOW_FILTER_EDUCATION_REQUIRED)
+  readData("employeetype", constant.SHOW_FILTER_EMPLOYEE_TYPE)
+  readData("experiencelevel", constant.SHOW_FILTER_EXPERIENCE_LEVEL)
+  readData("industry", constant.SHOW_FILTER_INDUSTRY)
+  readData("internalexternal", constant.SHOW_FILTER_INTERNAL_EXTERNAL)
+  readData("methodology", constant.SHOW_FILTER_METHODOLOGY)
+  readData("mobile", constant.SHOW_FILTER_MOBILE)
+  readData("accessibility", constant.SHOW_FILTER_ACCESSIBLE)
+  readData("petsallowed", constant.SHOW_FILTER_PETS_ALLOWED)
+  readData("dresscode", constant.SHOW_FILTER_DRESS_CODE)
+
   return (
     <div className="app-wrapper">
       <div className="app-body">
--- src/components/FilterTypeMenu/FilterTypeMenu.tsx
+++ src/components/FilterTypeMenu/FilterTypeMenu.tsx
@@ -1,8 +1,5 @@
-import { FC, useEffect, useState } from "react"
+import { FC } from "react"
 import { useStore } from "../../store/StoreProvider"
-import { TFilterDataArray } from "../../utils/types"
-import * as constant from "../../utils/constants"
-import axios from "axios"
 import clsx from "clsx"
 import "./FilterTypeMenu.scss"
 
@@ -17,48 +14,7 @@ interface Props {
 }
 
 const FilterTypeMenu: FC<Props> = ({ showFilter }) => {
-  const { filterData, setFilterData } = useStore()
-  const [count, setCount] = useState<number>(0)
-
-  const jsonData: TFilterDataArray = []
-
-  useEffect(() => {
-    if (count > 17) {
-      return
-    }
-    const readData = async (data: string, index: number) => {
-      try {
-        const response = await axios.get(`${constant.URL_DATA}${data}.json`)
-        jsonData[index] = await response.data
-        console.log(jsonData)
-        setCount(count + 1)
-        console.log(count)
-        if (count === 17) {
-          setFilterData(jsonData)
-        }
-      } catch (error) {
-        console.log(error)
-      }
-    }
-
-    readData("techstack", constant.SHOW_FILTER_TECH_STACK)
-    readData("workingfromhome", constant.SHOW_FILTER_WORKING_FROM_HOME)
-    readData("payment", constant.SHOW_FILTER_PAYMENT)
-    readData("benefits", constant.SHOW_FILTER_BENEFITS)
-    readData("commute", constant.SHOW_FILTER_COMMUTE)
-    readData("company", constant.SHOW_FILTER_COMPANY)
-    readData("companysize", constant.SHOW_FILTER_COMPANY_SIZE)
-    readData("educationrequired", constant.SHOW_FILTER_EDUCATION_REQUIRED)
-    readData("employeetype", constant.SHOW_FILTER_EMPLOYEE_TYPE)
-    readData("experiencelevel", constant.SHOW_FILTER_EXPERIENCE_LEVEL)
-    readData("industry", constant.SHOW_FILTER_INDUSTRY)
-    readData("internalexternal", constant.SHOW_FILTER_INTERNAL_EXTERNAL)
-    readData("methodology", constant.SHOW_FILTER_METHODOLOGY)
-    readData("mobile", constant.SHOW_FILTER_MOBILE)
-    readData("accessibility", constant.SHOW_FILTER_ACCESSIBLE)
-    readData("petsallowed", constant.SHOW_FILTER_PETS_ALLOWED)
-    readData("dresscode", constant.SHOW_FILTER_DRESS_CODE)
-  }, [])
+  const { filterData } = useStore()
 
   return (
     <div className="filter-type-menu">
