5accb30ace2fe94bb5aab4c31457a1936e914604
Patrick Coston <patcoston@gmail.com>
2021-03-10 11:26:12 -0500

    App: Refactored 17 calls to readData() into one using array.map()

--- src/App.tsx
+++ src/App.tsx
@@ -31,43 +31,30 @@ const App = () => {
       }
     }
 
-    const p1 = readData("techstack")
-    const p2 = readData("workingfromhome")
-    const p3 = readData("payment")
-    const p4 = readData("benefits")
-    const p5 = readData("commute")
-    const p6 = readData("company")
-    const p7 = readData("companysize")
-    const p8 = readData("educationrequired")
-    const p9 = readData("employeetype")
-    const p10 = readData("experiencelevel")
-    const p11 = readData("industry")
-    const p12 = readData("internalexternal")
-    const p13 = readData("methodology")
-    const p14 = readData("mobile")
-    const p15 = readData("accessibility")
-    const p16 = readData("petsallowed")
-    const p17 = readData("dresscode")
+    const dataName = [
+      "accessibility",
+      "benefits",
+      "commute",
+      "company",
+      "companysize",
+      "dresscode",
+      "educationrequired",
+      "employeetype",
+      "experiencelevel",
+      "experiencelevel",
+      "industry",
+      "internalexternal",
+      "methodology",
+      "mobile",
+      "payment",
+      "petsallowed",
+      "techstack",
+      "workingfromhome",
+    ]
 
-    Promise.all([
-      p1,
-      p2,
-      p3,
-      p4,
-      p5,
-      p6,
-      p7,
-      p8,
-      p9,
-      p10,
-      p11,
-      p12,
-      p13,
-      p14,
-      p15,
-      p16,
-      p17,
-    ]).then(() => {
+    const promises = dataName.map(label => readData(label))
+
+    Promise.all(promises).then(() => {
       setFilterData([...jsonData])
     })
   }, [])
