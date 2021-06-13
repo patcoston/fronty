5bc4422e899df1a9a42bcdf342fa2085aa32ea1e
Patrick Coston <patcoston@gmail.com>
2021-06-12 23:11:08 -0400

    Converted ShowActiveFilters from SCSS to JSS

--- src/components/ShowActiveFilters/ShowActiveFilters.tsx
+++ src/components/ShowActiveFilters/ShowActiveFilters.tsx
@@ -4,8 +4,8 @@ import { IFilters } from "../../utils/interfaces"
 import { TObjectEntries } from "../../utils/types"
 import CloseIcon from "../../svg/closeicon"
 import Button from "@material-ui/core/Button"
+import { makeStyles } from "@material-ui/core/styles"
 import clsx from "clsx"
-import "./ShowActiveFilters.scss"
 
 /**
  * Show Active Filters
@@ -13,11 +13,43 @@ import "./ShowActiveFilters.scss"
  * Purpose: Displays the filters which are currently active
  */
 
+const useStyles = makeStyles({
+  showActiveFilters: {
+    padding: 0,
+    margin: 0,
+  },
+  showActiveFiltersBody: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+  },
+  showActiveFiltersClose: {
+    padding: 0,
+    margin: "0 5px 0 0",
+    cursor: "pointer",
+  },
+  border: {
+    /* this is a global style defined in App.scss
+    put here temporarily */ border: "1px solid #ccc",
+  },
+  h3: {
+    marginLeft: 5,
+  },
+  section: {
+    padding: 0,
+    margin: 0,
+  },
+  button: {
+    margin: "5px 0 0 3px",
+  },
+})
+
 interface Props {
   activeFilterArray: TObjectEntries
 }
 
 const ShowActiveFilters: FC<Props> = ({ activeFilterArray }) => {
+  const classes = useStyles()
   const { activeFilters, setActiveFilters } = useStore()
 
   const handleDelete = (filter: string) => {
@@ -39,14 +71,14 @@ const ShowActiveFilters: FC<Props> = ({ activeFilterArray }) => {
   }
 
   return (
-    <div className="show-active-filters">
+    <div className={classes.showActiveFilters}>
       <strong>Current Filters: </strong> Click to toggle NOT filter
-      <div className={clsx("show-active-filters-body", "border")}>
+      <div className={clsx(classes.showActiveFiltersBody, classes.border)}>
         {activeFilterArray.map((filter, indexArray) => {
           return (
             <section key={indexArray}>
               <span
-                className="show-active-filters-close"
+                className={classes.showActiveFiltersClose}
                 onClick={() => handleDelete(filter[0])}
               >
                 <CloseIcon />
